0add155b2442b4494c51c13f3921171eb605957c
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2025-02-14 23:06:45 +0600

    comepleted

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth/web-extension";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
 
 const Login = () => {
@@ -7,6 +7,9 @@ const Login = () => {
     signInWithPopup(auth,provider)
     .then((result)=>{
       console.log(result)
+    })
+    .catch((error)=>{
+      console.log("Error",error)
     })
   }
   return (
